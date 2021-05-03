057c294cf6bda1cda2525353ffa09598ea952b50
Felipe Nunes <fnap@cin.ufpe.br>
2021-05-03 16:05:46 -0300

    add: linked list implementation

---
+++ Linked Lists/linked_lists.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+class Node {
+    public:
+        int data;
+        Node* next;
+
+    Node(int n){
+        this -> data = n;
+        this -> next = nullptr;
+    }
+};
+
+using namespace std;
+
+int main(){
+
+    Node* head = nullptr;
+    Node* second = nullptr;
+    Node* third = nullptr;
+
+    head = new Node(1);
+    second = new Node(2);
+    third = new Node(3);
+
+    head -> next = second;
+    second -> next = third;
+
+    return 0;
+}
